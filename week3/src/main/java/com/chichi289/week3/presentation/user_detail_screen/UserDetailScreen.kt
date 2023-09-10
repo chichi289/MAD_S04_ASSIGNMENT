70580ebbdfa21c4b114da2ad2aa9f226f556658b
chichi289 <chiragrprajapati28@gmail.com>
2023-09-10 18:17:24 +0530

    UI polish

--- week3/src/main/java/com/chichi289/week3/presentation/user_detail_screen/UserDetailScreen.kt
+++ week3/src/main/java/com/chichi289/week3/presentation/user_detail_screen/UserDetailScreen.kt
@@ -1,17 +1,19 @@
 package com.chichi289.week3.presentation.user_detail_screen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -33,24 +35,20 @@ fun UserDetailScreen(
     ) {
         Text(text = stringResource(R.string.txt_welcome_to_user_detail_screen))
 
-        Card(
-            elevation = CardDefaults.cardElevation(
-                defaultElevation = 1.dp
-            ),
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
                 .padding(start = 16.dp, top = 24.dp, end = 16.dp)
-                .padding(20.dp),
-            shape = RoundedCornerShape(12.dp),
-            colors = CardDefaults.cardColors(
-                containerColor = Color.LightGray
-            )
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(12.dp))
+                .background(Color.LightGray)
+                .padding(8.dp)
         ) {
-            CustomText(
-                key = stringResource(id = R.string.txt_userid), value = user.userId.toString()
-            )
+            CustomText(key = stringResource(id = R.string.txt_userid), value = user.userId.toString())
+            Spacer(modifier = Modifier.height(8.dp))
             CustomText(key = stringResource(id = R.string.txt_username), value = user.userName)
+            Spacer(modifier = Modifier.height(8.dp))
             CustomText(key = stringResource(id = R.string.txt_full_name), value = user.fullName)
+            Spacer(modifier = Modifier.height(8.dp))
             CustomText(key = stringResource(id = R.string.txt_email), value = user.email)
         }
 
