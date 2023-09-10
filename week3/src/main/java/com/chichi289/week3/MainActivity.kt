b963a9bfd8374dc4ccb54cb8f284a86dc85dfe05
chichi289 <chiragrprajapati28@gmail.com>
2023-09-10 13:37:24 +0530

    TopAppBar added

--- week3/src/main/java/com/chichi289/week3/MainActivity.kt
+++ week3/src/main/java/com/chichi289/week3/MainActivity.kt
@@ -4,9 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.compose.rememberNavController
 import com.chichi289.week3.navigation.MainGraph
 import com.chichi289.week3.ui.theme.MAD_S04_ASSIGNMENTTheme
@@ -14,6 +19,7 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -23,7 +29,15 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MainGraph(rememberNavController())
+                    Scaffold(
+                        topBar = {
+                            TopAppBar(title = {
+                                Text(text = stringResource(R.string.txt_user_directory))
+                            })
+                        }
+                    ) { paddingValues ->
+                        MainGraph(paddingValues, rememberNavController())
+                    }
                 }
             }
         }
--- week3/src/main/java/com/chichi289/week3/navigation/MainGraph.kt
+++ week3/src/main/java/com/chichi289/week3/navigation/MainGraph.kt
@@ -1,5 +1,7 @@
 package com.chichi289.week3.navigation
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
@@ -9,6 +11,7 @@ import com.chichi289.week3.presentation.user_list_screen.UserListScreen
 
 @Composable
 fun MainGraph(
+    paddingValues: PaddingValues,
     navController: NavHostController
 ) {
     NavHost(navController = navController, startDestination = Screen.USER_LIST_SCREEN.route) {
@@ -16,7 +19,7 @@ fun MainGraph(
 
         }
         composable(Screen.USER_LIST_SCREEN.route) {
-            UserListScreen(Modifier)
+            UserListScreen(Modifier.padding(paddingValues))
         }
         composable(Screen.USER_DETAIL_SCREEN.route) {
 
