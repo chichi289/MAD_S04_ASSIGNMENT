04d715ab42fd79936ff72813fc6867333b75694d
chichi289 <chiragrprajapati28@gmail.com>
2023-09-17 14:18:50 +0530

    - Different state of api response handled

--- week4/src/main/java/com/chichi289/week4/presentation/user_profile/UserProfileScreen.kt
+++ week4/src/main/java/com/chichi289/week4/presentation/user_profile/UserProfileScreen.kt
@@ -1,9 +1,12 @@
 package com.chichi289.week4.presentation.user_profile
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.chichi289.week4.data.remote.model.NetworkResult
+import com.chichi289.week4.data.remote.model.User
 import com.chichi289.week4.utils.log
 
 @Composable
@@ -11,13 +14,33 @@ fun UserProfileScreen(
     viewModel: UserProfileViewModel = hiltViewModel()
 ) {
 
-    val userDataState = remember {
+    val userDataState: State<NetworkResult<User>> = remember {
         viewModel.userStateFlow
     }.collectAsState()
 
-    userDataState.value.data?.let {
-        "Id:${it.id},Password:${it.password}".log()
+    when (userDataState.value) {
+        is NetworkResult.Loading -> {
+            "Loading".log()
+        }
+
+        is NetworkResult.Success -> {
+            "Success".log()
+            userDataState.value.data?.let {
+                "Id:${it.id},Password:${it.password}".log()
+            }
+        }
+
+        is NetworkResult.Error -> {
+            "Error".log()
+            userDataState.value.message.log()
+        }
+
+        is NetworkResult.NoInternetError -> {
+            "NoInternetError".log()
+            userDataState.value.message.log()
+        }
     }
+
 
 }
 
