23de4e3a1464356044067b1e4be7057d9aaf28b5
chichi289 <chiragrprajapati28@gmail.com>
2023-09-23 09:27:19 +0530

    - Week5 module added

---
+++ week5/src/main/java/com/chichi289/week5/presentation/login/LoginScreen.kt
@@ -0,0 +1,11 @@
+package com.chichi289.week5.presentation.login
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LoginScreen() {
+
+    Text(text = "Login")
+
+}
